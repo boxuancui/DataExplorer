16ab4a690c5b22fddbd7cb0793c3eaab07c8d533
Boxuan Cui <bcui@smartertravelmedia.com>
2016-03-01 13:48:08 -0500

    stable release to CRAN

--- R/DataExplorer-package.r
+++ R/DataExplorer-package.r
@@ -3,11 +3,11 @@
 #' @aliases DataExplorer dataexplorer DataExplorer-package
 #' @title Data Explorer
 #' @description Simplify and automate EDA process and report generation.
-#' @details EDA (Exploratory Data Analysis) process for data analysis and model
-#' building, so that users could focus on understanding data and extracting
-#' insights. The package automatically scans through each variable and does
-#' data profiling. Typical graphical techniques will be performed for both
-#' discrete and continuous features.
+#' @details Data exploration process for data analysis and model building, so
+#' that users could focus on understanding data and extracting insights. The
+#' package automatically scans through each variable and does data profiling.
+#' Typical graphical techniques will be performed for both discrete and
+#' continuous features.
 #' @seealso
 #' \link{PlotMissing},
 #' \link{BarDiscrete},
