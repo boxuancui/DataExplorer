c39304fd2c9d076bcde57391f8a7e689e974b724
Boxuan Cui <boxuancui@gmail.com>
2016-11-18 20:15:18 -0500

    Attempt 1 to fix directory issue for test-generate-report.r

--- tests/testthat/test-generate-report.r
+++ tests/testthat/test-generate-report.r
@@ -1,11 +1,10 @@
 context("generate report")
 
 test_that("test if report is generated", {
-  output_path <- file.path("tests", "testthat")
-  file_name <- "testthat_report.html"
-  file_dir <- file.path(output_path, file_name)
-  if (file.exists(file_dir)) file.remove(file_dir)
-  GenerateReport(iris, output_file = file_name, output_dir = output_path)
-  expect_true(file.exists(file.path(output_path, file_name)))
+  dir_name <- dirname(system.file("tests/testthat/test-generate-report.r", package = "DataExplorer"))
+  file_name <- "testthat-report.html"
+  file_dir <- file.path(dir_name, file_name)
+  GenerateReport(iris, output_file = file_name, output_dir = dir_name)
+  expect_true(file.exists(file_dir))
   expect_gte(file.info(file_dir)$size, 1700000)
 })
