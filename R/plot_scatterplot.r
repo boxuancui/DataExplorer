d9610346ce9bd2d6c808505b49c5e6a6f928230c
Boxuan Cui <boxuancui@gmail.com>
2018-01-01 01:29:55 -0500

    Updated vignettes; Fixed bug in plot_scatterplot

--- R/plot_scatterplot.r
+++ R/plot_scatterplot.r
@@ -12,29 +12,48 @@
 #' @export plot_scatterplot
 #' @seealso \link{geom_point}
 #' @examples
-#' # scatterplot mpg dataset by "hwy"
+#' # load mpg data from ggplot2
 #' data("mpg", package = "ggplot2")
-#' plot_scatterplot(mpg[, 1:10], "hwy", size = 1)
+#'
+#' # scatterplot mpg dataset by "hwy"
+#' plot_scatterplot(mpg, "hwy", size = 1)
+#' # scatterplot mpg dataset by "class"
+#' plot_scatterplot(mpg, "class", size = 1)
 
 plot_scatterplot <- function(data, by, title = NULL, ...) {
   ## Declare variable first to pass R CMD check
   variable <- NULL
   ## Check if input is data.table
   if (!is.data.table(data)) {data <- data.table(data)}
-  dt <- suppressWarnings(melt.data.table(data, id.vars = by))
-  ## Calculate number of pages
-  p <- ncol(data) - 1
-  pages <- ceiling(p / 9L)
-  ## Create ggplot object
-  for (pg in seq.int(pages)) {
-    col_names <- setdiff(names(data), by)[seq.int(9L * pg - 8L, min(p, 9L * pg))]
-    n_col <- ifelse(length(col_names) %% 3L, length(col_names) %/% 3L + 1L, length(col_names) %/% 3L)
-    plot <- ggplot(dt[variable %in% col_names], aes_string(x = by, y = "value")) +
-      geom_point(...) +
-      facet_wrap(~ variable, ncol = n_col, scales = "free_x", shrink = FALSE) +
-      coord_flip() +
-      xlab(by) + ggtitle(title) +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1))
-    print(plot)
+  ## Create plot function
+  scatterplot <- function(input_data, ...) {
+    dt <- suppressWarnings(melt.data.table(input_data, id.vars = by))
+    ## Calculate number of pages
+    p <- ncol(input_data) - 1
+    pages <- ceiling(p / 9L)
+    ## Create ggplot object
+    for (pg in seq.int(pages)) {
+      col_names <- setdiff(names(input_data), by)[seq.int(9L * pg - 8L, min(p, 9L * pg))]
+      n_col <- ifelse(length(col_names) %% 3L, length(col_names) %/% 3L + 1L, length(col_names) %/% 3L)
+      plot <- ggplot(dt[variable %in% col_names], aes_string(x = by, y = "value")) +
+        geom_point(...) +
+        facet_wrap(~ variable, ncol = n_col, scales = "free_x", shrink = FALSE) +
+        coord_flip() +
+        xlab(by) + ggtitle(title) +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1))
+      print(plot)
+    }
+  }
+  ## Call plot function
+  split_obj <- split_columns(data[, -by, with = FALSE])
+  if (split_obj$num_continuous > 0) {
+    plot_dt <- data.table(split_obj$continuous, "by_f" = data[[by]])
+    setnames(plot_dt, "by_f", by)
+    scatterplot(plot_dt, ...)
+  }
+  if (split_obj$num_discrete > 0) {
+    plot_dt <- data.table(split_obj$discrete, "by_f" = data[[by]])
+    setnames(plot_dt, "by_f", by)
+    scatterplot(plot_dt, ...)
   }
 }
