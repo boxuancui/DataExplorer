72e5a9d7ccaf7e1df3d5b34048994ce3a9e13948
Boxuan Cui <boxuancui@gmail.com>
2018-05-24 21:57:06 -0400

    Fixed memory usage bug in introduce()

--- R/introduce.r
+++ R/introduce.r
@@ -3,7 +3,17 @@
 #' Describe basic information for input data.
 #' @param data input data.
 #' @keywords introduce
-#' @return Describe basic information in a \link{data.frame}.
+#' @return Describe basic information in a \link{data.frame}:
+#' \itemize{
+#'   \item{rows: number of rows}
+#'   \item{columns: number of columns}
+#'   \item{discrete_columns: number of discrete columns}
+#'   \item{continuous_columns: number of continuous columns}
+#'   \item{all_missing_columns: number of columns with everything missing}
+#'   \item{total_missing_values: number of missing observations}
+#'   \item{total_observations: total number of observations}
+#'   \item{memory_usage: estimated memory allocation in bytes. See \link{object.size}}
+#' }
 #' @import data.table
 #' @importFrom utils object.size
 #' @export introduce
@@ -20,6 +30,6 @@ introduce <- function(data) {
     "all_missing_columns" = split_data[["num_all_missing"]],
     "total_missing_values" = sum(is.na(data)),
     "total_observations" = nrow(data) * ncol(data),
-    "size" = format(object.size(data), units = "auto")
+    "memory_usage" = as.numeric(object.size(data))
   )
 }
