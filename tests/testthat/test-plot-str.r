f22e52a4f387dce59c162aadb9deefe832159d46
Boxuan Cui <boxuancui@gmail.com>
2018-05-29 23:09:06 -0400

    Added unit test for #57

--- tests/testthat/test-plot-str.r
+++ tests/testthat/test-plot-str.r
@@ -1,5 +1,6 @@
 context("plot data structure")
 library(jsonlite)
+library(methods)
 
 test_that("test nested levels", {
   skip_on_cran()
@@ -15,4 +16,13 @@ test_that("test returned object is valid json", {
   obj <- list(lm(rnorm(5) ~ letters[1:5]), list(iris), list(list(list("a" = 1L))))
   obj_output <- plot_str(obj, print_network = FALSE)
   expect_true(validate(toJSON(obj_output)))
+})
+
+test_that("test S4 objects", {
+  setClass("DataExplorerPlotStrTest", representation("a" = "character", "b" = "numeric", "c" = "logical"))
+  test1 <- new("DataExplorerPlotStrTest", "a" = "test", "b" = 123, "c" = TRUE)
+  test2 <- new("DataExplorerPlotStrTest", "b" = 456, "c" = FALSE)
+  str_output <- plot_str(list(test1, test2), print_network = FALSE)
+  expect_equal(str_output$children[[1]]$children[[1]]$name, "@a (chr)")
+  expect_equal(str_output$children[[2]]$children[[3]]$name, "@c(name duplicated) (logi)")
 })
