bbb3eec68980e046699309b55e941a3706c70fcc
Boxuan Cui <boxuancui@gmail.com>
2023-02-14 11:49:55 -0500

    Fix PR#177

--- R/plot_missing.r
+++ R/plot_missing.r
@@ -3,6 +3,7 @@
 #' This function returns and plots frequency of missing values for each feature.
 #' @param data input data
 #' @param group missing profile band taking a list of group name and group upper bounds. Default is \code{list("Good" = 0.05, "OK" = 0.4, "Bad" = 0.8, "Remove" = 1)}.
+#' @param group_color bar color list for each of the group. Default is \code{list("Good" = "#1B9E77", "OK" = "#E6AB02", "Bad" = "#D95F02", "Remove" = "#E41A1C")}.
 #' @param missing_only plot features with missing values only? Default is \code{FALSE}.
 #' @param geom_label_args a list of other arguments to \link{geom_label}
 #' @param title plot title
@@ -19,6 +20,7 @@
 #' 
 #' ## Customize band
 #' plot_missing(airquality, group = list("B1" = 0, "B2" = 0.06, "B3" = 1))
+#' plot_missing(airquality, group = list("Awesome!" = 0.2, "Oh no!" = 1), group_color = list("Awesome!" = "green", "Oh no!" = "red"))
 #' 
 #' ## Shrink geom_label size
 #' library(ggplot2)
@@ -26,26 +28,12 @@
 
 plot_missing <- function(data,
                          group = list("Good" = 0.05, "OK" = 0.4, "Bad" = 0.8, "Remove" = 1),
-                         fill_color = c("Good"   = "#1B9E77",
-                                        "OK"     = "#E6AB02",
-                                        "Bad"    = "#D95F02",
-                                        "Remove" = "#E41A1C"),
+                         group_color = list("Good" = "#1B9E77", "OK" = "#E6AB02", "Bad" = "#D95F02", "Remove" = "#E41A1C"),
                          missing_only = FALSE,
                          geom_label_args = list(),
                          title = NULL,
                          ggtheme = theme_gray(),
                          theme_config = list("legend.position" = c("bottom"))) {
- 
-  ## Decide what color pallete to use 
-  if (length(group) != length(fill_color)| (mean(names(group) %in% names(fill_color)) != 1)) {
-      
-      message('Labels in "group" are different to labels in "fill_colors"')
-      message('-----Using default ggplot colors')
-      plot_fill <- scale_fill_discrete("Band")
-  }
-  else{
-      plot_fill <- scale_fill_manual(values = fill_color)
-  }
   
   ## Declare variable first to pass R CMD check
   num_missing <- pct_missing <- Band <- NULL
@@ -62,19 +50,23 @@ plot_missing <- function(data,
     }
   }))
   
-  
-  # Determine ordinal levels from group supplied
+  ## Determine ordinal levels from group supplied
   ordinal_levels <- names(group[sort.list(unlist(group))])
   
-  # Convert character to ordered factor to support ordinal legend
-  missing_value[, Band := factor(Band,
-                                 levels=ordinal_levels,
-                                 ordered = T)]
+  ## Convert character to ordered factor to support ordinal legend
+  missing_value[, Band := factor(Band, levels = ordinal_levels, ordered = TRUE)]
   
-## Create ggplot object
+  ## Determine bar fill color
+  if (length(setdiff(names(group), names(group_color))) > 0) {
+    bar_fill <- scale_fill_discrete("Band")
+  } else {
+    bar_fill <- scale_fill_manual(values = group_color)
+  }
+  
+  ## Create ggplot object
   output <- ggplot(missing_value, aes_string(x = "feature", y = "num_missing", fill = "Band")) +
     geom_bar(stat = "identity") +
-    plot_fill +
+    bar_fill +
     coord_flip() +
     xlab("Features") + ylab("Missing Rows") +
     guides(fill = guide_legend(override.aes = aes(label = "")))
