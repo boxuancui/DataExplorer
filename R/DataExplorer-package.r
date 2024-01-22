b4a4d6c6f71bea1f2ad5453f4657c3ecaab071b1
Boxuan Cui <boxuancui@gmail.com>
2024-01-22 15:47:29 -0500

    Update doc

--- R/DataExplorer-package.r
+++ R/DataExplorer-package.r
@@ -1,11 +1,6 @@
-#' @docType package
-#' @name DataExplorer-package
-#' @aliases DataExplorer dataexplorer DataExplorer-package
-#' @title Data Explorer
-#' @description Simplify and automate EDA process and report generation.
-#' @details Data exploration process for data analysis and model building, so
-#' that users could focus on understanding data and extracting insights. The
-#' package automatically scans through each variable and does data profiling.
-#' Typical graphical techniques will be performed for both discrete and
-#' continuous features.
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
 NULL
