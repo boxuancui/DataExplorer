8dad6f4958acccb7abc017134cfd3e52be8bcee9
Giovanni M Dall'Olio <giovanni@gmdbioinformatics.com>
2025-04-22 20:46:18 +0100

    Added support to ... in plot_histogram

--- R/plot_histogram.r
+++ R/plot_histogram.r
@@ -11,21 +11,16 @@
 #' @param nrow number of rows per page. Default is 4.
 #' @param ncol number of columns per page. Default is 4.
 #' @param parallel enable parallel? Default is \code{FALSE}.
+#' @param ... aesthetic mappings passed to \link{aes}, such as \code{fill = group}
 #' @return invisibly return the named list of ggplot objects
 #' @keywords plot_histogram
 #' @import data.table
 #' @import ggplot2
+#' @importFrom rlang enquos eval_tidy expr
 #' @export
 #' @seealso \link{geom_histogram} \link{plot_density}
 #' @examples
-#' # Plot iris data
-#' plot_histogram(iris, ncol = 2L)
-#'
-#' # Plot skewed data on log scale
-#' set.seed(1)
-#' skew <- data.frame(replicate(4L, rbeta(1000, 1, 5000)))
-#' plot_histogram(skew, ncol = 2L)
-#' plot_histogram(skew, scale_x = "log10", ncol = 2L)
+#' plot_histogram(iris, fill = Species, ncol = 2L)
 
 plot_histogram <- function(data, binary_as_factor = TRUE,
                            geom_histogram_args = list("bins" = 30L),
@@ -33,32 +28,46 @@ plot_histogram <- function(data, binary_as_factor = TRUE,
                            title = NULL,
                            ggtheme = theme_gray(), theme_config = list(),
                            nrow = 4L, ncol = 4L,
-                           parallel = FALSE) {
-  ## Declare variable first to pass R CMD check
+                           parallel = FALSE,
+                           ...) {
   variable <- value <- NULL
-  ## Check if input is data.table
+  
   if (!is.data.table(data)) data <- data.table(data)
-  ## Stop if no continuous features
+  
   split_data <- split_columns(data, binary_as_factor = binary_as_factor)
   if (split_data$num_continuous == 0) stop("No continuous features found!")
-  ## Get and reshape continuous features
+  
   continuous <- split_data$continuous
   feature_names <- names(continuous)
   dt <- suppressWarnings(melt.data.table(continuous, measure.vars = feature_names, variable.factor = FALSE))
-  ## Calculate number of pages
+  
+  # Replicate any non-measured columns from data into dt
+  other_vars <- setdiff(names(data), names(dt))
+  if (length(other_vars) > 0) {
+    dt <- cbind(
+      dt,
+      data[rep(seq_len(nrow(data)), times = length(feature_names)), ..other_vars]
+    )
+  }
+  
+  # Capture aesthetic mappings
+  aesthetic_quos <- enquos(...)
+  aes_combined <- eval_tidy(expr(aes(x = value, !!!aesthetic_quos)))
+  
   layout <- .getPageLayout(nrow, ncol, ncol(continuous))
-  ## Create ggplot object
+  
   plot_list <- .lapply(
     parallel = parallel,
     X = layout,
     FUN = function(x) {
-      ggplot(dt[variable %in% feature_names[x]], aes(x = value)) +
-        do.call("geom_histogram", c("na.rm" = TRUE, geom_histogram_args)) +
+      p <- ggplot(dt[variable %in% feature_names[x]], mapping = aes_combined) +
+        do.call("geom_histogram", c(list(na.rm = TRUE), geom_histogram_args)) +
         do.call(paste0("scale_x_", scale_x), list()) +
         ylab("Frequency")
+      p
     }
   )
-  ## Plot objects
+  
   class(plot_list) <- c("multiple", class(plot_list))
   plotDataExplorer(
     plot_obj = plot_list,
